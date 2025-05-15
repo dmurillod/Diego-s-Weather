b18704996884d6ad2c3973cbf20452c9374f022d
Diego Murillo <diegoa.murillod@gmail.com>
2025-05-15 17:32:07 -0500

    New improvements

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 import express from 'express';
 import axios from 'axios';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 3000;
+dotenv.config();
 
-const API_KEY = 'ba93f7758b026fbc16c5ed63c941d915';
+const API_KEY = process.env.API_KEY;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+function getCurrentDate() {
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dayName = daysOfWeek[dayOfWeek];
+  
+    const dayOfMonth = today.getDate();
+  
+    function getDaySuffix(day) {
+      if (day > 3 && day < 21) return "th";
+      switch (day % 10) {
+        case 1:
+          return "st";
+        case 2:
+          return "nd";
+        case 3:
+          return "rd";
+        default:
+          return "th";
+      }
+    }
+  
+    const daySuffix = getDaySuffix(dayOfMonth);
+    const monthIndex = today.getMonth();
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const monthName = months[monthIndex];
+  
+    return dayName + ", " + dayOfMonth + daySuffix + " " + monthName;
+  }
 
 async function getcoordinates(city){
     try{
@@ -30,9 +74,45 @@ async function getweather(lat, lon){
     }
 };
 
+async function getforecastify(lat, lon) {
+    try{
+        const response = await axios.get("https://api.openweathermap.org/data/2.5/" + `forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`);
+        return response;
+    } catch(error){
+        console.log(error.response.data);
+    }  
+};
+
 app.get('/', (req, res)=>{
     res.render("index.ejs");
 });
+
+app.get('/api/search', async (req, res) => {
+    const query = req.query.q;
+    if (!query) return res.status(400).json({ error: 'No city provided' });
+  
+    try {
+      const response = await axios.get('http://api.openweathermap.org/geo/1.0/direct', {
+        params: {
+          q: query,
+          limit: 5,
+          appid: process.env.API_KEY
+        }
+      });
+  
+      const data = response.data;
+  
+      if (!Array.isArray(data)) {
+        return res.status(500).json({ error: 'Unexpected response from OpenWeather' });
+      }
+  
+      res.json(data);
+    } catch (error) {
+      console.error('Error fetching from OpenWeather:', error.message);
+      res.status(500).json({ error: 'Error fetching city suggestions' });
+    }
+});
+  
 
 app.post('/submit', async (req, res)=>{
     const city = req.body['name-city'];
@@ -43,9 +123,16 @@ app.post('/submit', async (req, res)=>{
     console.log(longitud);
     const content = await getweather(latitud, longitud);
     console.log(content.data);
-    res.render("city.ejs", { content : content , location : location});
+    const forecast = await getforecastify(latitud, longitud);
+    console.log(forecast.data.list[0].weather[0].icon);
+    const currentDate = getCurrentDate();
+    const drops = Array.from({ length: 100 }, () => ({
+        left: (Math.random() * 100).toFixed(2),
+        delay: (Math.random() * 2).toFixed(2),
+      }));
+    res.render("city.ejs", { content : content.data , forecast : forecast.data, currentDate : currentDate, drops: drops});
 });
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
-});
+}); 
